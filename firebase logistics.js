14e9fab47599ff84cd82a376341de58437177bbc
ALEXPANAP <alex.h.pan@gmail.com>
2019-03-02 17:58:44 -0800

    Add files via upload

---
+++ firebase logistics.js
@@ -0,0 +1,35 @@
+   var users = 0;
+   var kwh = 0;
+   function round(value, decimals) {
+      return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+ }
+   function updateValues() {
+      firebase.database().ref('kwh').once('value', (snapshot) => {
+      kwh = snapshot.val();
+      document.getElementById("johNNy").innerHTML = round(kwh,3) +" KWH";
+      document.getElementById("joHnny").innerHTML = "$"+round(kwh*.12*365, 2);
+      console.log(kwh);
+    });
+
+    firebase.database().ref('users').once('value', (snapshot) => {
+      users = snapshot.val();
+      console.log(users);
+      document.getElementById("imagod").innerHTML = users +" people";
+    });
+   }
+   firebase.database().ref('kwh').on('value', function(snapshot) {
+      updateValues();
+   });   
+
+   firebase.database().ref('users').on('value', function(snapshot) {
+      updateValues();
+   });  
+   
+
+   function addUser(hours) {
+    firebase.database().ref('kwh').set(kwh + hours);
+    firebase.database().ref("users").set(++users);
+  }
+
+  updateValues();
+  
---
+++ process.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en-US">
+<html>
+<body>
+
+<head>
+<script language="javascript">
+     function multiply(){
+     var ans= (document.form.one.value)*(document.form.two.value)
+     document.form.answer.value=ans;
+     }
+     </script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="Description" content="Information on how much money and energy saved in Phantasma, leaderboards and mission statement">
+  <meta name="Keywords" content="Vampire energy, Money Saving, Phantasma">
+  <meta name="Author" content="Alex Pan & Sidney Hough">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <meta name = "application-name" content = "Phantasma">
+  <link rel="stylesheet" type="text/css"href="Styling.css">
+</head>
+
+<!--<button>About Us</button> 
+<button>Calculator</button>-->
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<p font-size:60px;>You have saved <?php echo $_POST["name"]; ?> &times; <?php echo $_POST["hours"]; ?>
+
+<!--h goes from 1-6 with 1 being biggest. Where h is determines where it is on webpage and # determines size-->
+
+
+<ul>
+    <li1><a style="font-size:40px;" class = "TITLE">Phantasma</a></li1>
+    <li><a></a></li>
+    <li><a></a></li>
+    <li><a></a></li>     
+    <li><a style="font-size:28.9px;" class = "button" href="Phantasma .html">Home</a></li>
+    <li><a style="font-size:28.9px;" class = "button" href="Calc.html">Calculator</a></li>
+    <li><a style="font-size:28.9px;" class = "button" href="about.html">About</a></li>
+</ul>
+
+<!--<a href="https://docs.google.com/document/d/1ComOiEkxwUSq5SZHDXkVDehPcXBImtgHlz2jwPEJ8JM/edit">This is a link</a> -->
+
+<!--<img title="I'm a tooltip" src="ww(5).jpg" alt="W3Schools.com" width="100" height="100"> -->
+
+
+
+</body>
+</html>
+
+
